e48d3381e116ac44c9f487c875ec10af2fa93f8d
Nitesh Vishvajeet Dubey <niteshd694@gmail.com>
2025-08-30 00:14:28 +0530

    02_variables.c

--- variables, datatypes & IO/02_variables.c
+++ variables, datatypes & IO/02_variables.c
@@ -3,11 +3,11 @@
 int main()
 {
     int number =25; //number variable stores 25
-    int _age=18; //_age variable stores 25
-    float pi=3.14; //pi variable stores 25
-    char star='*'; //star variable stores 25
-    int a=20; //a variable stores 25
-    int A=30; //A variable stores 25
+    int _age=18; //_age variable stores 18
+    float pi=3.14; //pi variable stores 3.14
+    char star='*'; //star variable stores *
+    int a=20; //a variable stores 20
+    int A=30; //A variable stores 30
     
 
     return 0;
