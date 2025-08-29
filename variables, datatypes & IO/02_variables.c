d1ab6f514c578397eaa10bfc414261d785e79b0f
Nitesh Vishvajeet Dubey <niteshd694@gmail.com>
2025-08-30 00:01:03 +0530

    Create 02_variables.c

---
+++ variables, datatypes & IO/02_variables.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+    int number =25; //number variable stores 25
+    int _age=18; //_age variable stores 25
+    float pi=3.14; //pi variable stores 25
+    char star='*'; //star variable stores 25
+    int a=20; //a variable stores 25
+    int A=30; //A variable stores 25
+    
+
+    return 0;
+}
