cbd7327085ff232a66c5ac262a5f14411185a695
Nitesh Vishvajeet Dubey <niteshd694@gmail.com>
2025-08-31 18:50:43 +0530

    03_datatype.c

---
+++ variables, datatypes & IO/03_datatype.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main()
+{
+    int age=18; //integer datatype
+    float pi=3.14; //real numbers (decimal values)
+    char name='n'; //character dataype
+
+    return 0;
+}
