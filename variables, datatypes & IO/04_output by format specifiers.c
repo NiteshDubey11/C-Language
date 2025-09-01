a9ef77d6c34722c59ce3cab3d1c7e212be66a3c3
Nitesh Vishvajeet Dubey <niteshd694@gmail.com>
2025-09-01 22:34:27 +0530

    Create 04_output by format specifiers .c

---
+++ variables, datatypes & IO/04_output by format specifiers.c
@@ -0,0 +1,17 @@
+/*output using format specifier*/
+
+
+#include <stdio.h>
+
+int main()
+{
+    int age=18;
+    char letter='n';
+    float pi=3.14;
+    printf("the age is %d\n",age);
+    printf("the letter is %c\n",letter);
+    printf("the real number is %f\n",pi);
+    
+
+    return 0;
+}
