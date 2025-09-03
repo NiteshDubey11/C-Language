e4e15751c3ad44e0cf800b713063b00702535e57
Nitesh Vishvajeet Dubey <niteshd694@gmail.com>
2025-09-03 22:27:30 +0530

    06_sum_of_two_numbers.c

---
+++ variables, datatypes & IO/06_sum_of_two_numbers.c
@@ -0,0 +1,18 @@
+//sum of two integers(a+b)
+
+#include <stdio.h>
+
+int main()
+{
+    int a,b,sum;
+    printf("enter value of a:");
+    scanf("%d",&a);
+    
+    printf("enter value of b:");
+    scanf("%d",&b);
+    
+    sum = a+b;
+    printf("the sum of two numbers a and b:%d",sum);
+
+    return 0;
+}
