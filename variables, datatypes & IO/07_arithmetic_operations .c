c83f9b5ff94111a07e5e3ab8b94dc406a9551927
Nitesh Vishvajeet Dubey <niteshd694@gmail.com>
2025-09-04 21:08:01 +0530

    07_arithmetic_operations .c

---
+++ variables, datatypes & IO/07_arithmetic_operations .c
@@ -0,0 +1,21 @@
+//arithemetic operations in lowest possible lines
+
+#include <stdio.h>
+
+int main()
+{
+   int a,b;
+   printf("enter value of A:");
+   scanf("%d",&a);
+   
+   printf("enter value of B:");
+   scanf("%d",&b);
+   
+   printf("The Summation of two numbers are : %d\n",a+b);
+   printf("The substraction of two numbers are : %d\n",a-b);
+   printf("The Multiplication of two numbers are : %d\n",a*b);
+   printf("The Division of two numbers are : %d",a/b);
+   
+
+    return 0;
+}
