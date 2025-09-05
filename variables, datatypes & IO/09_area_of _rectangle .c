5ba5349d51ba0b1e2ae069c3a71d3803db90a475
Nitesh Vishvajeet Dubey <niteshd694@gmail.com>
2025-09-05 22:32:09 +0530

    09_area_of _rectangle .c

---
+++ variables, datatypes & IO/09_area_of _rectangle .c
@@ -0,0 +1,13 @@
+//area of rectangle 
+#include <stdio.h>
+
+int main()
+{
+    int length,breadth;
+    
+    printf("enter length and breadth of rectangle:");
+    scanf("%d %d",&length,&breadth);
+    printf("area of rectangle:%d\n",length*breadth);
+    
+    return 0;
+}
