355f4bbb8aeab83a511b32daaf6c05a6e8a2f8b6
Nitesh Vishvajeet Dubey <niteshd694@gmail.com>
2025-09-05 22:35:52 +0530

    10_area_of_circle .c

---
+++ variables, datatypes & IO/10_area_of_circle .c
@@ -0,0 +1,14 @@
+//area of circle
+#include <stdio.h>
+
+int main()
+{
+    float radius;
+    float pi=3.14;
+    
+    printf("enter radius of circle:");
+    scanf("%f",&radius);
+    printf("area of circle:%f",pi*radius*radius);
+
+    return 0;
+}
