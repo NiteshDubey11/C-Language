30abcbd608ca81b90814ac0d16c9e997e3d57a02
Nitesh Vishvajeet Dubey <niteshd694@gmail.com>
2025-09-05 22:26:03 +0530

    08_area_of_square .c

---
+++ variables, datatypes & IO/08_area_of_square .c
@@ -0,0 +1,13 @@
+//area of square
+#include <stdio.h>
+
+int main()
+{
+    int side;
+    
+    printf("enter side of square:");
+    scanf("%d",&side);
+    printf("area of square:%d\n",side*side);
+    
+    return 0;
+}
