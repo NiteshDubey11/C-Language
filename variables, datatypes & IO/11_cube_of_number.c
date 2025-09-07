45926f183f6278cbdadc3a2557a661b5ed10933b
Nitesh Vishvajeet Dubey <niteshd694@gmail.com>
2025-09-07 23:19:04 +0530

    11_cube_of_number.c

---
+++ variables, datatypes & IO/11_cube_of_number.c
@@ -0,0 +1,15 @@
+//cube of a number (number*number*number)
+
+#include <stdio.h>
+
+int main()
+{
+    int a;
+    
+    printf("enter the number:");
+    scanf("%d",&a);
+    
+    printf("cube of number is:%d",a*a*a);
+
+    return 0;
+}
