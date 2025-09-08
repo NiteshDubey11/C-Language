da1915f41d1a8da87afd296e31abcc653d0927ba
Nitesh Vishvajeet Dubey <niteshd694@gmail.com>
2025-09-08 23:23:08 +0530

    01_type_declaration_instructions.c

---
+++ Instructions and Operators/01_type_declaration_instructions.c
@@ -0,0 +1,24 @@
+//type declaration instructions
+
+#include <stdio.h>
+
+int main()
+{
+    int age=18;
+    int oldage=age;
+    int newage=oldage+2;
+    
+    printf("new age:%d",newage);
+    
+    int rupee=86,dollar;
+    dollar=1;
+    
+    printf("\ndollar:%d",dollar);
+    
+    int age1,age2,age3;
+    age1=age2=age3=22;
+    
+    printf("\nage1:%d",age1);
+    
+    return 0;
+}
