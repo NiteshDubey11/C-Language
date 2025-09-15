9542bb43a82bf16dd8931e038e49f7a7f9c1e36f
Nitesh Vishvajeet Dubey <niteshd694@gmail.com>
2025-09-15 23:57:57 +0530

    07_relational_operators.c

---
+++ Instructions and Operators/07_relational_operators.c
@@ -0,0 +1,31 @@
+/*
+    Relational operators:
+    1. == (equals to)
+    2. >,>= (greater than,greater than or equal to)
+    3. <,<= (less than ,less than or equal to)
+    4. != (not equal to)
+
+    NOTE: 1 represnts TRUE,0 represents FALSE
+*/
+#include <stdio.h>
+
+int main()
+{
+    printf("%d",4==4); //returns 1 as 4 is equal to 4
+    printf("\n%d",4==3); //returns 0 as 4 is not equal to 3
+    
+    printf("\n%d",3>4); //returns 0 as 3 is not greater than 4
+    printf("\n%d",4>3); //returns 1 as 4 is greater than 3
+    printf("\n%d",4>4); //returns 0 as 4 is not greater than 4
+    printf("\n%d",4>=4); //returns 1 as 4 is equal to 4
+    
+    printf("\n%d",3<4); //returns 1 as 3 is less than 4
+    printf("\n%d",4<3); //returns 0 as 4 is not less than 3
+    printf("\n%d",3<3); //returns 0 as 3 is not less than 3
+    printf("\n%d",3<=3); //returns 1 as 3 is equal to  3
+    
+    printf("\n%d",4!=4); //returns 0 as 4 is not equal to 4 is false
+    printf("\n%d",4!=3); //returns 1 as 4 is not equal to 3 
+    
+    return 0;
+}
