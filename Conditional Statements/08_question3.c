cfa18bcb7f5769877f5a9f6bb99c659f26ffb9ba
Nitesh Vishvajeet Dubey <niteshd694@gmail.com>
2025-12-30 19:45:12 +0530

    08_question3.c

---
+++ Conditional Statements/08_question3.c
@@ -0,0 +1,21 @@
+/*
+    == stands for comparison
+    = stands for assignment
+    
+    therefore in if statement,x=2 assigns x as non zero value that is 2 and doesn't compares with 2.
+    
+    NOTE:In if statement nonzero value indicates TRUE and zero value indicates FALSE 
+*/
+
+#include <stdio.h>
+
+int main() {
+    int x=2;
+    if(x=1){
+        printf("x is equal to 1");
+    }
+    else{
+        printf("x is not equal to 1");
+    }
+    return 0;
+}
