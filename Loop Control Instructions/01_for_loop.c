5baf0a097168cafccff175e4d4b63c9ef45d1b7c
Nitesh Vishvajeet Dubey <niteshd694@gmail.com>
2025-12-31 22:26:26 +0530

    01_for_loop.c

---
+++ Loop Control Instructions/01_for_loop.c
@@ -0,0 +1,19 @@
+/*
+    Format for "for loop instruction"
+    
+    for(initialisation;condition;updation){
+        //do something
+    }
+
+    //Code for printing "Hello World!!!" 10 times
+    
+*/
+#include <stdio.h>
+
+int main() {
+    for(int i=1;i<=10;i++){
+        printf("Hello world!!! \n");
+    }
+
+    return 0;
+}
