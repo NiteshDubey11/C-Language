f224eb4407ae242af2648655d465eb91e443cb9b
Nitesh Vishvajeet Dubey <niteshd694@gmail.com>
2026-01-02 19:52:09 +0530

    05_while_loop.c

---
+++ Loop Control Instructions/05_while_loop.c
@@ -0,0 +1,20 @@
+/*
+    Format for "While Loop Instruction":
+    
+    while(condition){
+        //do something
+    }
+    
+    //Code for printing "Hello World!!!" 10 times
+    
+*/
+#include <stdio.h>
+
+int main() {
+    int i=1;
+    while(i<=10){
+        printf("Hello World!!!\n");
+        i++;
+    }
+    return 0;
+}
