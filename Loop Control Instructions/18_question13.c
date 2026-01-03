9fd757f95aca37f090a3449756948faa5e1d13f6
Nitesh Vishvajeet Dubey <niteshd694@gmail.com>
2026-01-03 16:43:07 +0530

    18_question13.c

---
+++ Loop Control Instructions/18_question13.c
@@ -0,0 +1,14 @@
+/*
+    Question 13:
+    Calculate the sum of all numbers between 5 and 50 (including 5 and 50)
+*/
+#include <stdio.h>
+
+int main() {
+    int sum=0;
+    for(int i=5;i<=50;i++){
+    sum=sum+i;
+    }
+printf("Sum:%d",sum);
+    return 0;
+}
