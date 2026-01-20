a22eadf2809dad9fe986c85376262b20e32dfc9c
Nitesh Vishvajeet Dubey <niteshd694@gmail.com>
2026-01-20 13:07:18 +0530

    03_value_at_address.c

---
+++ Pointers/03_value_at_address.c
@@ -0,0 +1,15 @@
+/*
+    Example of value at address operator (*ptr)
+*/
+#include <stdio.h>
+
+int main() {
+    int age=22;
+    int *ptr=&age;
+    
+    printf("%d\n",age);
+    printf("%d\n",*ptr);
+    printf("%d\n",*(&age));
+
+    return 0;
+}
