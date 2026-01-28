385fabd5e5dc77fb80a289e2aca1f8fe33dbab3b
Nitesh Vishvajeet Dubey <niteshd694@gmail.com>
2026-01-28 20:05:56 +0530

    02_question1.c

---
+++ Arrays/02_question1.c
@@ -0,0 +1,19 @@
+/*
+    Question 1 :
+    Write a program to enter price of 3 items & print their final cost with GST.
+*/
+
+#include <stdio.h>
+
+int main() {
+    float price[3];
+    printf("enter prices of 3 items : ");
+    scanf("%f",&price[0]);
+    scanf("%f",&price[1]);
+    scanf("%f",&price[2]);
+    
+    printf("Total price of first item with GST is %.2f\n",price[0]+(0.18*price[0]));
+    printf("Total price of second item with GST is %.2f\n",price[1]+(0.18*price[1]));
+    printf("Total price of third item with GST is %.2f\n",price[2]+(0.18*price[2]));
+    return 0;
+}
