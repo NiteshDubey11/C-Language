7af33de4ce525cdac2959493a3dc7c3cf30a8ffc
Nitesh Vishvajeet Dubey <niteshd694@gmail.com>
2026-01-28 09:38:26 +0530

    10_question5.c

---
+++ Pointers/10_question5.c
@@ -0,0 +1,29 @@
+/*
+    Question 5 :
+    //will the address of the output be same ?
+    
+    void printAddress(int n);
+    int main(){
+        int n = 4;
+        printf("%p\n",&n) ;
+        printAddress(n);
+        return 0;
+    }
+    void printAddress(int n){
+        printf("%p\n",&n);
+    }
+*/
+#include <stdio.h>
+
+void printAddress(int n);
+
+int main(){
+    int n=4;
+    printf("%p\n",&n) ;
+    printAddress(n);
+    return 0;
+}
+
+void printAddress(int n) {
+    printf("%p\n", &n);
+}
