70cff933e98fb2f4e0091d5cee5bf553d69f68c4
Nitesh Vishvajeet Dubey <niteshd694@gmail.com>
2026-07-01 23:45:15 +0530

    array reversal method-1
    Implemented functions to reverse an array and print its elements.

---
+++ Arrays/09_question3.c
@@ -0,0 +1,31 @@
+/*
+    Question 3:
+    Write a function to reverse an array.
+*/
+
+#include<stdio.h>
+
+void reverse(int arr[],int n);
+void printarr(int arr[],int n);
+
+void main(){
+    int arr[]={1,2,3,4};
+    reverse(arr,4);
+    printarr(arr,4);
+    
+}
+
+void reverse(int arr[],int n){
+    for(int i=0;i<n/2;i++){
+        int firstnumber=arr[i];
+        int secondnumber=arr[n-i-1];
+        arr[i]=secondnumber;
+        arr[n-i-1]=firstnumber;
+    }
+}
+
+void printarr(int arr[],int n){
+    for(int i=0;i<n;i++){
+        printf("%d\t",arr[i]);
+    }
+}
