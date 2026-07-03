00e5f9864ff32f2f16e0930111173a1db102cfc2
Nitesh Vishvajeet Dubey <niteshd694@gmail.com>
2026-07-03 22:11:32 +0530

    character array and string
    Added a C program demonstrating string and character array usage.

---
+++ Strings/01_string.c
@@ -0,0 +1,23 @@
+/*
+    String: A character array terminated by a '\0'(null character).
+    null character denotes string termination(end)
+
+*/
+
+#include<stdio.h>
+
+int main(){
+    char name[]={'N','I','T','E','S','H'};   //array of characters
+    char _name[]={'N','I','T','E','S','H','\0'};   //string
+    
+    for(int i=0;i<6;i++){
+        printf("%c",name[i]);
+    }
+    
+    printf("\n");
+    
+    for(int i=0;i<6;i++){
+        printf("%c",_name[i]);
+    }
+    
+}
